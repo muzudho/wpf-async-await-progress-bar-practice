1cc50904c1422fbcc1db689a3ef86baa3b16acbf
むずでょ <muzudho1@gmail.com>
2021-10-14 19:50:02 +0900

    処理２　うまく行かない例

--- WpfAsyncAwaitProgressBarPractice/WpfAsyncAwaitProgressBarPractice/MainWindow.xaml.cs
+++ WpfAsyncAwaitProgressBarPractice/WpfAsyncAwaitProgressBarPractice/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace WpfAsyncAwaitProgressBarPractice
 {
+    using System;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using WpfAsyncAwaitProgressBarPractice.Views;
@@ -16,7 +18,7 @@
         }
 
         /// <summary>
-        /// [同期処理１（うまく行かない例）]ボタン押下時
+        /// [処理１ 同期（うまく行かない例）]ボタン押下時
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -30,13 +32,13 @@
             subWindow.Show();
 
             // ここで時間のかかる処理を行います（うまく動きません）
-            DoWork(subWindow.progressBar1);
+            Do10Works(subWindow.progressBar1);
 
             // サブウィンドウを閉じます
             subWindow.Close();
         }
 
-        private static void DoWork(ProgressBar progressBar)
+        private static void Do10Works(ProgressBar progressBar)
         {
             const int kTotal = 10;
             for (int i = 0; i < kTotal; i++)
@@ -46,6 +48,53 @@
 
                 progressBar.Value = i * 100 / kTotal;
             }
+        }
+
+        /// <summary>
+        /// [処理２ 同期（うまく行かない例）]ボタン押下時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Sync2Button_Click(object sender, RoutedEventArgs e)
+        {
+            // サブウィンドウ（プログレスバーが置いてあります）を表示します
+            var subWindow = new ProgressBarWindowView();
+            subWindow.textBlock1.Text = "このメッセージは表示されますが、\nプログレスバーにアクセスできません";
+
+            // サブウィンドウを表示します
+            subWindow.Show();
+
+            // この Sync2Button_Click メソッドが終わらないと、
+            // 別ウィンドウのプログレスバーを描画するということもできません
+
+            var func = () =>
+            {
+                // そこで、プログレスバーの更新と、ウィンドウを閉じることを
+                // 別スレッドで行います
+
+                // ここで時間のかかる処理を行います（うまく動きません）
+                // このスレッドは subWindowオブジェクト を持っていないので、（元のスレッドが持っている）
+                // アクセスできません
+                try
+                {
+                    Do10Works(subWindow.progressBar1);
+                }
+                catch (InvalidOperationException e)
+                {
+                    MessageBox.Show("例外が発生しました", "[処理２ 同期（うまく行かない例）]ボタン押下時", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return $"処理２ 未完了";
+                }
+
+                // ここは通りません
+
+                // サブウィンドウを閉じます
+                subWindow.Close();
+
+                return $"処理２ 完了";
+            };
+
+            // 待機しません
+            Task.Run(func);
         }
     }
 }
